7fb0182f57ded47b2ecdb9237a941285dc87e97a
Eunice Amorim <up201904920@fe.up.pt>
2022-05-02 15:31:25 +0100

    Jasmin integrated in main

---
+++ test/pt/up/fe/comp/OurOllirToJasminTest.java
@@ -0,0 +1,52 @@
+package pt.up.fe.comp;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import pt.up.fe.comp.TestUtils;
+import pt.up.fe.specs.util.SpecsIo;
+import pt.up.fe.specs.util.SpecsStrings;
+
+public class OurOllirToJasminTest {
+
+    @Test
+    public void fac(){
+        //TODO -> tradução OllirParaJasmin
+        String jasminCode = "";
+
+        var output = TestUtils.runJasmin(jasminCode);
+        assertEquals("Output\n", SpecsStrings.normalizeFileContents(output));
+    }
+
+    @Test
+    public void myclass1(){
+        String jasminCode = "";
+
+        var output = TestUtils.runJasmin(jasminCode);
+        assertEquals("Output\n", SpecsStrings.normalizeFileContents(output));
+    }
+
+    @Test
+    public void myclass2(){
+        String jasminCode = "";
+
+        var output = TestUtils.runJasmin(jasminCode);
+        assertEquals("Output\n", SpecsStrings.normalizeFileContents(output));
+    }
+
+    @Test
+    public void myclass3(){
+        String jasminCode = "";
+
+        var output = TestUtils.runJasmin(jasminCode);
+        assertEquals("Output\n", SpecsStrings.normalizeFileContents(output));
+    }
+
+    @Test
+    public void myclass4(){
+        String jasminCode = "";
+
+        var output = TestUtils.runJasmin(jasminCode);
+        assertEquals("Output\n", SpecsStrings.normalizeFileContents(output));
+    }
+}
