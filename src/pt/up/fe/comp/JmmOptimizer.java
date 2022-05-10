af2a86ea48a1f207713c9b8fe59a1f8f52a270f3
Lucas Calvet Santos <lucascalvet2000@hotmail.com>
2022-05-10 22:35:15 +0100

    Added OLLIR generator visitor

--- src/pt/up/fe/comp/JmmOptimizer.java
+++ src/pt/up/fe/comp/JmmOptimizer.java
@@ -1,8 +1,12 @@
 package pt.up.fe.comp;
 
 import pt.up.fe.comp.jmm.analysis.JmmSemanticsResult;
+import pt.up.fe.comp.jmm.analysis.table.JmmSymbolTable;
 import pt.up.fe.comp.jmm.ollir.JmmOptimization;
+import pt.up.fe.comp.jmm.ollir.OllirGenerator;
 import pt.up.fe.comp.jmm.ollir.OllirResult;
+
+import java.util.Collections;
 
 public class JmmOptimizer implements JmmOptimization {
     @Override
@@ -12,7 +16,13 @@ public class JmmOptimizer implements JmmOptimization {
 
     @Override
     public OllirResult toOllir(JmmSemanticsResult semanticsResult) {
-        return null;
+        OllirGenerator ollirGenerator = new OllirGenerator(new JmmSymbolTable(semanticsResult.getRootNode()));
+        ollirGenerator.visit(semanticsResult.getRootNode());
+        String ollirCode = ollirGenerator.getCode();
+
+        System.out.println("OLLIR CODE:\n" + ollirCode);
+
+        return new OllirResult(semanticsResult, ollirCode, Collections.emptyList());
     }
 
     @Override
