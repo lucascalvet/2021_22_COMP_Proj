89d0045598cab158449f4cb0fef2b66ab08ae699
Eunice Amorim <up201904920@fe.up.pt>
2022-06-10 14:33:19 +0100

    Test jasmin simple if fixed

--- src-lib/pt/up/fe/comp/jmm/jasmin/ConversionInstructions.java
+++ src-lib/pt/up/fe/comp/jmm/jasmin/ConversionInstructions.java
@@ -54,31 +54,38 @@ public class ConversionInstructions {
             result.append(op11);
             result.append("ifne ");
             result.append(instruction.getLabel()).append("\n");
+        } else {
+
+            Element operand1 = instruction.getOperands().get(0);
+            Element operand2 = instruction.getOperands().get(1);
+
+            String op1 = stackHandle.load(operand1, scope);
+            String op2 = stackHandle.load(operand2, scope);
+            Instruction operation = instruction.getCondition();
+
+            BinaryOpInstruction op = (BinaryOpInstruction) operation;
+            OperationType type = op.getOperation().getOpType();
+
+            //availar a expressção
+            switch (type){
+                case LTH:
+                    result.append(lthConversion(op1, op2));
+                    break;
+                case ANDB:
+                    result.append(andConversion(op1, op2));
+                    break;
+                case NOTB:
+                    result.append(notConversion(op1));
+                    break;
+                default:
+                    throw new NotImplementedException(this);
+            }
+
+            result.append("ifne ");
+            result.append(instruction.getLabel()).append("\n");
+
         }
-/*
-        Element operand1 = instruction.getOperands().get(0);
-        Element operand2 = instruction.getOperands().get(1);
 
-        String op1 = stackHandle.load(operand1, scope);
-        String op2 = stackHandle.load(operand2, scope);
-        Instruction operation = instruction.getCondition();
-
-        BinaryOpInstruction op = (BinaryOpInstruction) operation;
-        OperationType type = op.getOperation().getOpType();
-
-        switch (type){
-            case LTH:
-                result.append(lthConversion(op1, op2));
-                break;
-            case ANDB:
-                result.append(andConversion(op1, op2));
-                break;
-            case NOTB:
-                result.append(notConversion(op1));
-                break;
-            default:
-                throw new NotImplementedException(this);
-        }*/
 
         return result.toString();
     }
